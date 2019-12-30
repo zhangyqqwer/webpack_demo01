617a71ebc9d2e4358800c7ba6235fd5212c6e469
FengZhongNaLvYouShang <424015308@qq.com>
2019-12-30 17:42:21 +0800

    11111

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import $ from 'jquery' // 导入jquery 使用$变量来接收
 //导入样式表
 import './css/index.css'
-
+//
 $(function () {
     $('li:odd').css('backgroundColor','yellow')
     $('li:even').css('backgroundColor',function () {
